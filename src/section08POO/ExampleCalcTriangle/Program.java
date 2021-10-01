589453b3ef08b488f0bde805d8dc6c1e7aff8dc8
Peter Leite <peterleite.dev@gmail.com>
2021-10-01 08:11:46 -0300

    Exercicio Funções Calculando area triangulo

---
+++ src/section08POO/ExampleCalcTriangle/Program.java
@@ -0,0 +1,31 @@
+package section08POO.ExampleCalcTriangle;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Enter the measures of triangle X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Enter the measures of triangle Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.printf("Area x = %.2f", areaX);
+        System.out.printf("%n Area Y = %.2f", areaY);
+/*
+        System.out.println("Area x = " + areaX);
+        System.out.println("Area y = " + areaY);
+*/
+    }
+}
---
+++ src/section08POO/ExampleCalcTriangle/Triangle.java
@@ -0,0 +1,13 @@
+package section08POO.ExampleCalcTriangle;
+
+import java.util.Scanner;
+
+public class Triangle {
+        public double a;
+        public double b;
+        public double c;
+        public double area() {
+            double p = (a + b + c) / 2.0;
+            return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+}

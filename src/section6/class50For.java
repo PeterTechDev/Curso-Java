b964d9ce2cab03d0f694e9a496fcbeebfdda0afa
Peter Leite <peterleite.dev@gmail.com>
2021-10-01 06:24:25 -0300

    exercicios JAVA: while e for

--- src/section6/class50For.java
+++ src/section6/class50For.java
@@ -1,4 +1,22 @@
 package section6;
 
+import java.util.Scanner;
+
 public class class50For {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número de entradas a serem somadas");
+        int N = sc.nextInt();
+
+        int soma = 0;
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            soma = soma + x;
+        }
+        System.out.println("resultado: " + soma);
+
+        sc.close();
+
+    }
 }
